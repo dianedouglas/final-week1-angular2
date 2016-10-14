aa7db56a32403ad6705058c6f4452136ed30a120
Gibberish314 <a.moosen@gmail.com>
2016-10-14 00:36:56 -0400

    Display a task.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,13 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
+    <h3>{{ task.description }}</h3>
   </div>
   `
 })
 
 export class AppComponent { 
-
+  public task: Task = new Task("Create To-Do List app.", 0);
 }
 
 export class Task {
