888fb7478571e2c2db8fc191c3354ee517a4b7c1
Gibberish314 <a.moosen@gmail.com>
2016-10-14 01:16:20 -0400

    Add *ngIf directive and done button to hide the edit form when the user clicks the done button.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,14 +9,17 @@ import { Component } from '@angular/core';
       <h3>{{ currentTask.description }}</h3>
       <button (click)="showDetails(currentTask)">Edit</button>
     </div>
-    <h1>Edit Task</h1>
-    <div>
-      <label>Enter Task Description: </label>
-      <input [(ngModel)]="selectedTask.description">
-    </div>
-    <div>
-      <label>Enter Task ID:</label>
-      <input [(ngModel)]="selectedTask.id">
+    <div *ngIf="selectedTask">
+      <h1>Edit Task</h1>
+      <div>
+        <label>Enter Task Description: </label>
+        <input [(ngModel)]="selectedTask.description">
+      </div>
+      <div>
+        <label>Enter Task ID:</label>
+        <input [(ngModel)]="selectedTask.id">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
@@ -32,6 +35,9 @@ export class AppComponent {
   selectedTask: Task = this.tasks[0];
   showDetails(clickedTask: Task) {
     this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
