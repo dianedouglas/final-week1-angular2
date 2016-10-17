2f6ab706270ca654e644e4a6092b4333c1a484eb
Gibberish314 <a.moosen@gmail.com>
2016-10-17 15:12:47 -0400

    Add basic pipe to task list to only display tasks that are not complete.

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
 
+
 @Component({
   selector: 'task-list',
   template: `
-    <div *ngFor="let currentTask of childTaskList">
+    <div *ngFor="let currentTask of childTaskList | completeness">
       <h3>{{ currentTask.description }}</h3>
       <button (click)="editButtonHasBeenClicked(currentTask)">Edit</button>
     </div>
